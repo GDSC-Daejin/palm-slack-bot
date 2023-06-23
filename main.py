99613b29fa4787d40b8ef8acc8f482bd313744f1
dntjd1097 <dntjd1097@gmail.com>
2023-06-24 02:10:44 +0900

    error handling fixed

--- main.py
+++ main.py
@@ -12,41 +12,46 @@ app = App(token=bot_token, signing_secret=signing_secret)
 client = WebClient(token=bot_token)
 
 
-def send_message(channel, text, thread_ts=None):
+def send_message(channel, eng, kor, thread_ts=None):
     try:
-        response = client.chat_postMessage(channel=channel, text=text, thread_ts=thread_ts)
+        response = client.chat_postMessage(channel=channel, text=kor, thread_ts=thread_ts)
         return response
     except SlackApiError as e:
+        print(text, e)
         text = f"Error sending message: {e.response['error']}"
-        client.chat_postMessage(channel=channel, text=text, thread_ts=thread_ts)
+        response = client.chat_postMessage(channel=channel, text=eng, thread_ts=thread_ts)
 
 
 def processing_prompt(prompt):
+    result = ""
+
     def split_text(text):
-        pattern = r"```[\s\S]*?```"  # 백틱으로 된 코드 블록을 찾는 정규식 패턴
+        pattern = r"```[\s\S]*?```"  # Regular expression pattern to find code blocks enclosed in backticks
         code_blocks = re.findall(pattern, text)
         sentences = re.split(pattern, text)
-
         return sentences, code_blocks
 
-    def listToString(str_list):
-        result = ""
-        for s in str_list:
-            result += s + " "
-        return result.strip()
+    def list_to_string(str_list):
+        return " ".join(str_list).strip()
 
-    generated_text = PALM_BOT.generate_text(text_to_eng(prompt))
+    eng_result = PALM_BOT.generate_text(text_to_eng(prompt))
+    if isinstance(eng_result, dict):
+        eng_result = str(eng_result)
+        return eng_result, eng_result
+    else:
+        sentences, code_blocks = split_text(eng_result)
+        kor_sentences = [text_to_kor(sentence) for sentence in sentences]
 
-    sentences, code_blocks = split_text(generated_text)
-    kor_sentences = []
-    for sentence in sentences:
-        text_to_korean = text_to_kor(sentence)
-        kor_sentences.append(text_to_korean)
-    merged_list = [
-        kor_sentence + code_block for kor_sentence, code_block in zip(kor_sentences, code_blocks)
-    ]
-    result = listToString(merged_list)
-    return result
+        merged_list = []
+        for i, kor_sentence in enumerate(kor_sentences):
+            if i < len(code_blocks):
+                merged_list.append(kor_sentence + code_blocks[i])
+            else:
+                # Handling the case where there are more kor_sentences than code_blocks
+                merged_list.append(kor_sentence)
+
+        kor_result = list_to_string(merged_list)
+        return eng_result, kor_result
 
 
 @app.event("message")
@@ -57,8 +62,8 @@ def handle_message_event(event):
     channel = event["channel"]
     # DM 이벤트인지 확인
     if channel_type == "im":
-        prompt = processing_prompt(text)
-        send_message(channel=channel, text=prompt, thread_ts=ts)
+        eng, kor = processing_prompt(text)
+        send_message(channel=channel, eng=eng, kor=kor, thread_ts=ts)
         # client.chat_postMessage(channel=channel, text=prompt, thread_ts=ts)
 
 
@@ -68,8 +73,8 @@ def handle_mention(body, say, logger, event):
     text = re.sub(pattern, "", event["text"]).strip()
     ts = event["ts"]
     channel = event["channel"]
-    prompt = processing_prompt(text)
-    send_message(channel=channel, text=prompt, thread_ts=ts)
+    eng, kor = processing_prompt(text)
+    send_message(channel=channel, eng=eng, kor=kor, thread_ts=ts)
 
 
 if __name__ == "__main__":

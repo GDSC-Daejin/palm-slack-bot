2026e89cf906bcbb21b0195348a07f14c576dd42
dntjd1097 <dntjd1097@gmail.com>
2023-08-06 15:01:09 +0900

    헛소리 차단

--- ai/palm.py
+++ ai/palm.py
@@ -18,7 +18,7 @@ class PALM:
             completion = palm.generate_text(
                 model=self.model,
                 prompt=prompt,
-                temperature=1.0,
+                temperature=0.5,
                 max_output_tokens=self.models[0].output_token_limit,
             )
             if completion.result is None:
